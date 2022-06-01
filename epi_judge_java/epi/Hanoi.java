49c8d070111c021cb76222911990fc0352b343ab
Andrew Schroeder <aschroed@ualberta.ca>
2022-05-31 19:36:45 -0600

    Towers of Hanoi solution

--- epi_judge_java/epi/Hanoi.java
+++ epi_judge_java/epi/Hanoi.java
@@ -4,19 +4,44 @@ import epi.test_framework.GenericTest;
 import epi.test_framework.TestFailure;
 import epi.test_framework.TimedExecutor;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Deque;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 public class Hanoi {
 
   private static final int NUM_PEGS = 3;
 
   public static List<List<Integer>> computeTowerHanoi(int numRings) {
-    // TODO - you fill in here.
-    return Collections.emptyList();
+    List<Deque<Integer>> pegs = IntStream.range(0, NUM_PEGS).mapToObj(i-> new ArrayDeque<Integer>()).collect(Collectors.toList());
+    List<List<Integer>> result = new ArrayList<>();
+
+    // Initialize the first peg
+    for(int i=numRings; i>0; i--){
+      pegs.get(0).addFirst(i);
+    }
+
+    int from = 0, to = 1, using = 2;
+    computeHanoiOperations(result, pegs, from, to, using, numRings);
+
+    return result;
   }
+
+  private static void computeHanoiOperations(List<List<Integer>> result, List<Deque<Integer>> pegs, int from, int to, int using, int numRings) {
+
+    if(numRings > 0){
+      // Step 1: Move all but the last ring from peg 1 to peg 3 using 2
+      computeHanoiOperations(result, pegs, from, using, to, numRings-1);
+
+      // Step 2: Move the last ring from 1 to 2, add operation to result
+      pegs.get(to).addFirst(pegs.get(from).getFirst());
+      result.add(List.of(from, to));
+
+      // Step 3: Move the remaining rings from 3 to 2 using 1
+      computeHanoiOperations(result, pegs, using, to, from, numRings-1);
+    }
+  }
+
   @EpiTest(testDataFile = "hanoi.tsv")
   public static void computeTowerHanoiWrapper(TimedExecutor executor,
                                               int numRings) throws Exception {
